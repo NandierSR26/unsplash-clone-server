2d8d4daf58faa4488b7e04c07687cd33ffeb997f
Nandier Ruiz <nandiersruiz@outlook.com>
2022-11-28 22:48:20 -0500

    cors configured

--- app.js
+++ app.js
@@ -10,21 +10,21 @@ const port = process.env.PORT
 dbConecction();
 
 // Configurar CORS
-const whitelist = [process.env.FRONTEND_URL, 'http://localhost:5173'];
+// const whitelist = [process.env.FRONTEND_URL, 'http://localhost:5173'];
 
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.includes(origin)) {
-      // Puede consultar la API
-      callback(null, true);
-    } else {
-      // No esta permitido
-      callback(new Error("Error de Cors"));
-    }
-  },
-};
+// const corsOptions = {
+//   origin: function (origin, callback) {
+//     if (whitelist.includes(origin)) {
+//       // Puede consultar la API
+//       callback(null, true);
+//     } else {
+//       // No esta permitido
+//       callback(new Error("Error de Cors"));
+//     }
+//   },
+// };
 
-app.use(cors(corsOptions))
+app.use(cors())
 app.use(express.json())
 app.use( express.static('public') )
 
