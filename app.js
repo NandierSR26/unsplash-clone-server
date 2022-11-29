d0512f2d38091808d1d8c83f125597d57d413fa7
Nandier Ruiz <nandiersruiz@outlook.com>
2022-11-29 17:03:55 -0500

    configuration in cors

--- app.js
+++ app.js
@@ -10,28 +10,28 @@ const port = process.env.PORT
 dbConecction();
 
 // Configurar CORS
-// const whitelist = [process.env.FRONTEND_URL, 'http://localhost:5173'];
+const whitelist = [process.env.FRONTEND_URL, process.env.FRONTEND_URL_2, 'http://localhost:5173'];
 
-// const corsOptions = {
-//   origin: function (origin, callback) {
-//     if (whitelist.includes(origin)) {
-//       // Puede consultar la API
-//       callback(null, true);
-//     } else {
-//       // No esta permitido
-//       callback(new Error("Error de Cors"));
-//     }
-//   },
-// };
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (whitelist.includes(origin)) {
+            // Puede consultar la API
+            callback(null, true);
+        } else {
+            // No esta permitido
+            callback(new Error("Error de Cors"));
+        }
+    },
+};
 
-app.use(cors())
+app.use(cors(corsOptions))
 app.use(express.json())
-app.use( express.static('public') )
+app.use(express.static('public'))
 
 app.use(fileUpload({
-    useTempFiles : true,
-    tempFileDir : './uploads',
-    createParentPath : true
+    useTempFiles: true,
+    tempFileDir: './uploads',
+    createParentPath: true
 }));
 
 app.use('/api/auth', require('./routes/auth.routes'));
@@ -39,5 +39,5 @@ app.use('/api/pictures', require('./routes/pictures.routes'));
 app.use('/api/users', require('./routes/users.routes'));
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${ port }`);
+    console.log(`Server is running on port ${port}`);
 })
